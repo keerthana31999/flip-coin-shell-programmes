884145687f0372d27b90853cff66185ef35e69a1
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 21:51:01 +0530

    Added::win count

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,9 +1,19 @@
-#!/bin/bash -x
-flip=$((RANDOM%2))
-if (( $flip==1 ))
-then
-	echo "head"
-else
-	echo "tails"
-fi
 
+#!/bin/bash -x
+coutHeadWin=0
+countTailsWin=0
+for (( i=1; i<=20; i++ ))
+do
+	flip=$((RANDOM%2))
+
+	if (( $flip==1 ))
+	then
+		eecho "head win"
+		((countHeadWin++))
+	else
+		echo "tails"
+		((countTailsWin++))
+	fi
+done
+echo "head win count out of 20 is=" $countHeadWin
+echo "tails win count out of 20 is=" $countTailsWin
